9ecc0f94f45fe659d8943dc0f3cc04238cf108f8
Anton Fox <42889325+AntonFoxy@users.noreply.github.com>
2023-04-28 08:15:05 +0300

    Добавьте файлы проекта.

---
+++ Classes/DogApiClass.cs
@@ -0,0 +1,9 @@
+﻿
+namespace dogApi.Classes
+{
+    internal class DogApiClass
+    {
+        public string message { get; set; }
+        public string status { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using dogApi.Classes;
+using Newtonsoft.Json;
+
+namespace dogApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest request = WebRequest.Create($"https://dog.ceo/api/breeds/image/random");
+            WebResponse response = request.GetResponse();
+            StreamReader streamReader = new StreamReader(response.GetResponseStream());
+
+            var obj = JsonConvert.DeserializeObject<DogApiClass>(streamReader.ReadToEnd());
+
+            try
+            {
+                if (obj.status == "success")
+                {
+                    Console.WriteLine($"Status: {obj.status}, Link: {obj.message}");
+                    Process.Start(obj.message);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
